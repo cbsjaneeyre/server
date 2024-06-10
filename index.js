bae7a9b86780128e36f2e7458283116bd5bb23af
Margarita Zolotareva <glam-dreamer@yandex.ru>
2024-06-11 00:13:09 +0300

    env file added

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 
 // yarn start and curl http://localhost:3000/date to run the server
 
+require('dotenv').config();
+
 const express = require('express');
 
 const app = express();
 
-const limit = 10;
-const interval = 1000;
 const PORT = 3000;
 
 let connections = [];
@@ -25,7 +25,7 @@ const currentDate = new Date().toUTCString();
 setTimeout(function run () {
   console.log(timer);
 
-  if (++timer > limit) {
+  if (++timer > process.env.limit) {
     connections.map(res => {
       res.write(`terminal stopped running on: ${currentDate}.\n`);
       res.end();
@@ -39,8 +39,8 @@ setTimeout(function run () {
     res.write(`current date is: ${currentDate}.\n`);
   });
 
-  setTimeout(run, interval);
-}, interval);
+  setTimeout(run, process.env.interval);
+}, process.env.interval);
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
